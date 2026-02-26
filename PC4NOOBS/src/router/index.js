93d0e46680806d9c69812977fec286c6d3abf1c7
Alessio-Huber <alessio.huber@hotmail.com>
2026-02-26 15:19:08 +0100

    account page added closed #20

--- PC4NOOBS/src/router/index.js
+++ PC4NOOBS/src/router/index.js
@@ -24,10 +24,14 @@ import ImprintView from '@/views/legal/ImprintView.vue';
 import TermsView from '@/views/legal/TermsView.vue';
 import PrivacyView from '@/views/legal/PrivacyView.vue';
 
+import AccountView from '@/views/AccountView.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", name: "home", component: HomeView },
+
+    { path: "/account", name: "account", component: AccountView },
 
     { path: "/imprint", name: "imprint", component: ImprintView },
     { path: "/terms", name: "terms", component: TermsView },
---
+++ PC4NOOBS/src/views/AccountView.vue
@@ -0,0 +1,41 @@
+<template>
+    <main>
+        <section>
+            <h1>Account Details</h1>
+
+            <div>
+                <!--Profile picture-->
+                <figure>
+                    <img src="/src/assets/avatar.png" alt="Profile picture">
+                </figure>
+
+                <!--Account information-->
+                <section>
+                    <header>
+                        <h2>Personal Information</h2>
+                        <button type="button">Edit</button>
+                    </header>
+
+                    <ul>
+                        <li><strong>Name:</strong> John Doe</li>
+                        <li><strong>Email:</strong> john.doe@example.com</li>
+                        <li><strong>Phone:</strong> +41 79 345 67 89</li>
+                        <li><strong>Date of birth:</strong> 31.12.2000</li>
+                    </ul>
+
+                    <h2>Delivery Address</h2>
+                    <ul>
+                        <li><strong>Street:</strong> Blächikonstrasse 3</li>
+                        <li><strong>City / ZIP:</strong> Bounarge 9659</li>
+                    </ul>
+
+                    <h2>Selected Language</h2>
+                    <p>German</p>
+
+                    <h2>Costumer ID</h2>
+                    <p>123456789</p>
+                </section>
+            </div>
+        </section>
+    </main>
+</template>
