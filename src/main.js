c13f35eeeb0f166f5f0b314a2dbac3977c75d536
Ioannis Ligkas <Ioannis_Ligkas@sluz.ch>
2026-05-01 21:20:25 +0200

    commiting new gitignore

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 const { loadEnvFile } = require("node:process");
-loadEnvFile("./.env");
+loadEnvFile("../.env");
 
 const app = createApp(App)
 
